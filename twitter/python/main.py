86dd83a2ee10a3ca18b59faa9318c094b98346d4
barmstrong <barmstrong@cfday.net>
2021-07-30 08:27:10 -0400

    Added more features to the twitter miner: can convert multivalue fields into strings with a delimiter (if the repeated types are too hard to work with); can specify a path within the buckets to use (if you change from using delimited strings to arrays). Also fixed an issue that prevented the debug value from being used. Changed from -log to -debug in the command-line interface.

--- twitter/python/main.py
+++ twitter/python/main.py
@@ -41,7 +41,8 @@ class ExampleRequest(object):
   args = None
   
   # (args.projectId,args.query,limit=args.limit,topic=args.topic,debug=args.log)
-  def __init__(self, projectId, query, limit=None, topic=None, userTopic=None, bucket=None, userBucket=None,
+  def __init__(self, projectId, query, limit=None, topic=None, userTopic=None, bucket=None, userBucket=None, pathInBuckets=None,
+               delim=None,
                debug=None):
     if type(query) == str:
       if not query.startswith('"'): query = '"' + query + '"'
@@ -52,6 +53,9 @@ class ExampleRequest(object):
                'bucket': bucket if bucket is not None else '',
                'userTopic': userTopic if userTopic is not None else '',
                'userBucket': userBucket if userBucket is not None else ''}
+    if pathInBuckets is not None: message['path']=pathInBuckets
+    if delim is not None: message['delim']=delim
+    if debug is not None: message['debug']=debug
     self.args = {'message': json.dumps(message)}
   
   def get_json(self, force=False):
@@ -103,6 +107,8 @@ class MyListener(StreamListener):
                  "listed_count", "favourites_count", "statuses_count", "created_at", "following", "follow_request_sent",
                  "notifications"]
   
+  _multivalueTweetFields=["coordinates", "hashtags", "user_mentions", "symbols", "extended_tweet"]
+  
   @classmethod
   def extractReference(cls, outerField, element):
     '''
@@ -122,7 +128,7 @@ class MyListener(StreamListener):
     return entities
   
   @classmethod
-  def extractTweet(cls, tweet, query):
+  def extractTweet(cls, tweet, query, delim=None):
     '''
     Will process a single tweet and produce one or more records of tweet data. A single tweet may reference a tweet that it is retweeting, in which case this method returns both as tweet data records.
     :param tweet: the JSON from twitter representing one tweet.
@@ -156,13 +162,26 @@ class MyListener(StreamListener):
         
         if field == 'retweeted_status':
           try:
-            nestedTweets = cls.extractTweet(tweet['retweeted_status'], query)
+            nestedTweets = cls.extractTweet(tweet['retweeted_status'], query,delim=delim)
             tweetRows.extend(nestedTweets)
           except:
             _logger.error('SKIPPING Cannot parse nested tweet ' + str(tweet['retweeted_status']),exc_info=True,stack_info=True)
     tweetRow['query'] = query
-    tweetRow['text'] = json.dumps(tweet)
-    tweetRows.append(tweetRow)
+    if delim is None:
+      # Convert empty fields for the multivalue fields into empty arrays for BigQuery since REPEATED type fields cannot be null.
+      for multivalueField in MyListener._multivalueTweetFields:
+        if multivalueField not in tweetRow or tweetRow[multivalueField] is None: tweetRow[multivalueField]=[]
+    tweetRow['raw'] = json.dumps(tweet)
+    if delim is not None:
+      # Convert multivalue fields into strings with values separated by delim.
+      tweetRows.append(dict(map(lambda item:
+                               (
+                                 item[0],
+                                 delim.join(map(str,item[1])) if type(item[1])==list else item[1]
+                               ),
+                               tweetRow.items())))
+    else:
+      tweetRows.append(tweetRow)
     return tweetRows
   
   @classmethod
@@ -191,7 +210,21 @@ class MyListener(StreamListener):
         userRows.extend(cls.extractUsers(element))
     return userRows
   
-  def __init__(self, projectId, query, limit, topic=None, userTopic=None, bucket=None, userBucket=None):
+  def __init__(self, projectId, query, limit, topic=None, userTopic=None, bucket=None, userBucket=None, pathInBucket=None, delim=None, debug=None):
+    '''
+    :param projectId:
+    :param query:
+    :param limit:
+    :param topic:
+    :param userTopic:
+    :param bucket:
+    :param userBucket:
+    :param pathInBucket:
+    :param delim:
+    :param debug:
+    '''
+    if debug is not None: _logger.setLevel(min(debug,_logger.level))
+    
     self.query = query
     self.limit = limit
     if topic is not None:
@@ -211,6 +244,7 @@ class MyListener(StreamListener):
     self._publisher = None
     self._userPublisher = None
     
+    self._path=pathInBucket
     self._bucketClient = None
     self._userBucketClient = None
     self._bucket = bucket
@@ -219,6 +253,8 @@ class MyListener(StreamListener):
     self._userBucket = userBucket
     if userBucket is not None:
       _logger.debug('Output user data to bucket: ' + self._userBucket)
+      
+    self._delim=delim
   
   def _writeToBucket(self, bucketClient, bucket, records):
     key = self._createObjectKey()
@@ -254,13 +290,13 @@ class MyListener(StreamListener):
     else:
       key += '_'.join(self.query)
     key += '_' + str(datetime.datetime.now()) + '.json'
-    return re.sub(r'[^A-Za-z0-9_.-]', '_', key)
+    return ('' if self._path is None else self._path+'/')+re.sub(r'[^A-Za-z0-9_.-]', '_', key)
   
   def on_data(self, data):
     print(json.dumps({'log': 'Found tweet...'}))
     try:
       tweets = json.loads(data)
-      tweetRecords = self.extractTweet(tweets, self.query)
+      tweetRecords = self.extractTweet(tweets, self.query, delim=self._delim)
       userRecords = self.extractUsers(tweets)
       
       if self._bucket is not None:
@@ -300,15 +336,6 @@ class MyListener(StreamListener):
     time.sleep(10)
     return True
 
-def format_filename(fname):
-  """Convert file name into a safe string.
-  Arguments:
-      fname -- the file name to convert
-  Return:
-      String -- converted file name
-  """
-  return re.sub(r'[^A-Za-z0-9_.-]', r'_', fname) + '_' + datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S.%f')
-
 def main(request):
   """Responds to any HTTP request.
   Args:
@@ -319,24 +346,28 @@ def main(request):
       `make_response <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>`.
   """
   messageJSON = _getMessageJSON(request)
-  
+
   debug=messageJSON.get('debug', None)
   if debug is not None: _logger.setLevel(debug)
-  
+
+  _logger.info('Trigger message received is ' + json.dumps(messageJSON))
+
   limit = messageJSON.get('limit', None)
-  if limit == '': limit = 10
+  if limit is None or str(limit) == '': limit = 10
   
   query = messageJSON.get('query', ['The Fast Dog'])
   _logger.info('Query is "{query}", limit is set to {limit}.'.format(query=query,limit=str(limit) if limit is not None else 'max'))
   
+  pathInBuckets=messageJSON.get('path',None)
+  
   bucket = messageJSON.get('bucket', None)
   if bucket == '': bucket = None
   if bucket is not None:
-    _logger.info('Using {bucket} for storing tweets.'.format(bucket=bucket))
+    _logger.info('Using gs://{bucket}{pathInBucket} for storing tweets.'.format(bucket=bucket,pathInBucket='' if pathInBuckets is None else '/'+pathInBuckets))
   userBucket = messageJSON.get('userBucket', None)
   if userBucket == '': userBucket = None
   if userBucket is not None:
-    _logger.info('Using {bucket} for storing users.'.format(bucket=userBucket))
+    _logger.info('Using gs://{bucket}{pathInBucket} for storing users.'.format(bucket=userBucket,pathInBucket='' if pathInBuckets is None else '/'+pathInBuckets))
 
   projectId = messageJSON.get('projectId', '')
   if projectId == '': projectId = None
@@ -356,7 +387,8 @@ def main(request):
       return 'Error attempting to access Pub/Sub topic with no project ID.'
     _logger.info('Will submit users to {topic}.'.format(topic=userTopic))
 
-  _logger.info('Trigger message received is ' + json.dumps(messageJSON))
+  delim=messageJSON.get('delim',None)
+  if delim is not None: _logger.info('Will output multivalue fields as strings delimited by "{delim}".'.format(delim=delim))
   
   if type(query) == str: query = [query]
   
@@ -364,18 +396,22 @@ def main(request):
   # Read key and secret from file.
   with open('twitterKeys.json') as twitterKeyFile:
     keys=json.load(twitterKeyFile)
+  
+  requiredKeys=['consumer_key','consumer_secret','access_token','access_secret']
+  if any(filter(lambda key:key not in keys,requiredKeys)):
+    _logger.error('Cannot read required keys from twitterKeys.json. This file must exist and have the format {"consumer_key":"...","consumer_secret":"...","access_token":"...","access_secret":"..."}.')
+    return 'Cannot read required keys from twitterKeys.json'
   twitterAuth = OAuthHandler(keys['consumer_key'], keys['consumer_secret'])
   twitterAuth.set_access_token(keys['access_token'], keys['access_secret'])
   
   for term in query:
-    _logger.debug('Waiting on rate limit to replenish.')
+    _logger.debug('Checking rate limit. Will potentially wait until rate limit replenishes...')
     tweepy.API(twitterAuth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
     
     _logger.debug('Querying for {term}'.format(term=term))
     twitter_stream = Stream(twitterAuth, MyListener(projectId, term, limit, topic=topic, userTopic=userTopic, bucket=bucket,
-                                             userBucket=userBucket))
+                                             userBucket=userBucket,pathInBucket=pathInBuckets,delim=delim,debug=debug))
     twitter_stream.filter(track=term)
-    _logger.debug('Done querying for {query}.'.format(query=term))
   return json.dumps(messageJSON)+' completed.'
 
 if __name__ == '__main__':
@@ -385,7 +421,7 @@ if __name__ == '__main__':
   # add arguments to the parser
   parser.add_argument('-query', nargs='+', help='One or more terms to search form (enclose phrases in double quotes.)')
   parser.add_argument('-limit', nargs='?', help='Limit the number of calls to twitter per query term, default is 10.',
-                      default=None)
+                      default=None,type=int)
   parser.add_argument('-bucket',
                       help='Provide a bucket name to store the twitter data to if you want to persist the data in GCS. The bucket must already exist.',
                       default=None)
@@ -401,12 +437,19 @@ if __name__ == '__main__':
   parser.add_argument('-userTopic',
                       help='Specify a pub/sub topic to publish to if you want the user records to go to pub/sub.',
                       default=None)
-  parser.add_argument('-log', help='Print out log statements.', default=None)
+  parser.add_argument('-delim',
+                      help='Specify a delimiter to use for multivalue fields. This causes multivalue fields to be string type instead of arrays.',
+                      default=None)
+  parser.add_argument('-path',
+                      help='Place all tweets and users within the given path (in the tweet and user buckets).',
+                      default=None)
+  parser.add_argument('-debug', help='Print out log statements.', default=None, type=int)
   
   # parse the arguments
   args = parser.parse_args()
   
   exampleRequest = ExampleRequest(args.projectId, args.query, limit=args.limit, topic=args.topic,
-                                  userTopic=args.userTopic, bucket=args.bucket, userBucket=args.userBucket,
-                                  debug=args.log)
+                                  userTopic=args.userTopic, bucket=args.bucket, userBucket=args.userBucket, pathInBuckets=args.path,
+                                  delim=args.delim,
+                                  debug=args.debug)
   main(exampleRequest)
